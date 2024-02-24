3685cf82f6022613c58347e91672639db9512920
Your Name <you@example.com>
2024-02-24 17:06:12 +0900

    LoginView、LogoutView、UserProfileViewの実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 
 from .forms import SignupForm
 
@@ -17,3 +18,7 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
+
+
+class UserProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "accounts/profile.html"
