88b3963a5d3997996b7da4062d73e43ac1274dc7
Your Name <you@example.com>
2024-02-25 17:34:25 +0900

    テストの実装

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,5 +128,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.User"
 
 LOGIN_URL = "accounts:login"
-LOGOUT_URL = "accounts:logout"
 LOGIN_REDIRECT_URL = "tweets:home"
+LOGOUT_REDIRECT_URL = "accounts:login"
